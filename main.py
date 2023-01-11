5d48fdb4bfda6f26f144cb3dfa2d345a08913f25
mariaclarin <maria.clarin@binus.ac.id>
2023-01-11 21:01:54 +0700

    redirected main to landingPage()

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
     root.resizable(False, False)
 
     # To fill in the window with widgets from frames.py
-    frames.addBooks(root)
+    frames.landingPage(root)
 
     # To run the window
     root.mainloop()
